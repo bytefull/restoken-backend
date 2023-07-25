0fcc4d1e5d1110d6deb4e85e74ecedc810141019
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2023-07-25 18:15:50 +0100

    cleaning house

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def on_startup():
-    # Not needed if you setup a migration system like Alembic
-    print('call await create_db_and_tables() here')
-
 @app.get("/")
 async def root():
     return {"Hello": "World"}
-
-@app.get("/api/v1/users")
-async def fetch_users():
-    return []
